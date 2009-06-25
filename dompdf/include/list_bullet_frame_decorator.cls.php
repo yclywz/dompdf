8ebec7ba50a049de342f101c5ca76b2605cf477d
helmuttis <helmuttis@8e70de24-3a84-11de-b438-597f59cd7555>
2009-06-25 01:01:57 +0000

    In certain cases modify position of images as bullets. More test cases.

--- dompdf/include/list_bullet_frame_decorator.cls.php
+++ dompdf/include/list_bullet_frame_decorator.cls.php
@@ -68,13 +68,16 @@ class List_Bullet_Frame_Decorator extends Frame_Decorator {
   }
   
   function get_margin_width() {
-    // Small hack to prevent indenting of list text
-    if ( $this->_frame->get_style()->list_style_position == "outside" )
+    $style = $this->_frame->get_style();
+    // Small hack to prevent extra indenting of list text on list_style_position == "inside"
+    // and on suppressed bullet
+    if ( $style->list_style_position == "outside" ||
+         $style->list_style_type == "none" )
       return 0;
-    return $this->_frame->get_style()->get_font_size()*self::BULLET_SIZE + 2 * self::BULLET_PADDING;
+    return $style->get_font_size()*self::BULLET_SIZE + 2 * self::BULLET_PADDING;
   }
 
-  //hits only on "inset" lists items, to increase hight of box
+  //hits only on "inset" lists items, to increase height of box
   function get_margin_height() {
     return $this->_frame->get_style()->get_font_size()*self::BULLET_SIZE + 2 * self::BULLET_PADDING;
   }
